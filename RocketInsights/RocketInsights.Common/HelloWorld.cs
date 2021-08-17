7a38faf72ec92f687080d8abae9f95e0dd0ed19c
John Askew <john.askew@deptagency.com>
2021-08-17 17:23:40 -0400

    Add solution with library and unit testing.

---
+++ RocketInsights/RocketInsights.Common/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿namespace RocketInsights.Common
+{
+    public class HelloWorld
+    {
+        public string Foo { get; set; }
+        public string Bar { get; set; }
+
+        public string Concatenate()
+        {
+            return Foo + Bar;
+        }
+    }
+}
---
+++ RocketInsights/RocketInsights.Tests/HelloWorldUnitTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RocketInsights.Common;
+
+namespace RocketInsights.Tests
+{
+    [TestClass]
+    public class HelloWorldUnitTests
+    {
+        [TestMethod]
+        public void Concatenation()
+        {
+            var model = new HelloWorld
+            {
+                Foo = "foo",
+                Bar = "bar"
+            };
+
+            var concatenated = model.Concatenate();
+
+            Assert.AreEqual("foobar", concatenated);
+        }
+    }
+}
