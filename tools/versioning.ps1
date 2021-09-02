65330e69cf1feb039d5a83545e98407dcda505b1
John Askew <john.askew@deptagency.com>
2021-09-02 11:06:22 -0400

    Update versioning to support recursively updating all csproj files.

--- tools/versioning.ps1
+++ tools/versioning.ps1
@@ -1,29 +1,37 @@
-$csprojPath = $args[0] # path to the file
+$rootPath = $args[0] # path to search for *.csproj
 $versionPatch = $args[1] # the patch version
 
 Write-Host "Appending patch to major & minor version in C# Project"
 
-$filePath = $csprojPath
-$xml=New-Object XML
-$xml.Load($filePath)
+$projects = Get-ChildItem -Path $rootPath -Filter '*.csproj' -File -recurse
 
-$versionNode = $xml.Project.PropertyGroup.Version
+foreach ($project in $projects) {
 
-if ($versionNode -eq $null) {
-	# If you have a new project and have not changed the version number the Version tag may not exist
-	$versionNode = $xml.CreateElement("Version")
-    $versionNode.InnerText = "0.0.0";
-	$xml.Project.PropertyGroup.AppendChild($versionNode)
-	Write-Host "Version XML tag added to the csproj"
+	Write-Host $project
+
+	$filePath = $project.FullName
+
+	$xml = New-Object XML
+	$xml.Load($filePath)
+
+	$versionNode = $xml.Project.PropertyGroup.Version
+
+	if ($null -eq $versionNode) {
+		# If you have a new project and have not changed the version number the Version tag may not exist
+		$versionNode = $xml.CreateElement("Version")
+		$versionNode.InnerText = "0.0.0";
+		$xml.Project.PropertyGroup.AppendChild($versionNode)
+		Write-Host "Version XML tag added to the csproj"
+	}
+
+	$versionParts = $xml.Project.PropertyGroup.Version.Split(".")
+	$versionMajor = $versionParts[0]
+	$versionMinor = $versionParts[1]
+
+	$versionUpdated = $versionMajor + "." + $versionMinor + "." + $versionPatch
+	Write-Host $versionUpdated
+
+	$xml.Project.PropertyGroup.Version = $versionUpdated
+
+	$xml.Save($filePath)
 }
-
-$versionParts = $xml.Project.PropertyGroup.Version.Split(".")
-$versionMajor = $versionParts[0]
-$versionMinor = $versionParts[1]
-
-$versionUpdated = $versionMajor + "." + $versionMinor + "." + $versionPatch
-Write-Host $versionUpdated
-
-$xml.Project.PropertyGroup.Version = $versionUpdated
-
-$xml.Save($filePath)
