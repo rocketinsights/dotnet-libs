e5d6c91b8033769f41d816c7e82ff0e9750b06db
John Askew <john.askew@deptagency.com>
2022-07-13 21:11:01 -0400

    Add Kontent provider

---
+++ src/RocketInsights.DXP.Providers.Kontent/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using RocketInsights.DXP.Providers.Kontent;
+using RocketInsights.DXP.Services;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddKontent(this IServiceCollection services)
+        {
+            services.AddSingleton<ILayoutService, KontentService>();
+            services.AddSingleton<IContentService, KontentService>();
+
+            return services;
+        }
+    }
+}
---
+++ src/RocketInsights.DXP.Providers.Kontent/KontentService.cs
@@ -0,0 +1,30 @@
+﻿using RocketInsights.DXP.Models;
+using RocketInsights.DXP.Services;
+using System.Threading.Tasks;
+
+namespace RocketInsights.DXP.Providers.Kontent
+{
+    public class KontentService : ILayoutService, IContentService
+    {
+        public Task<Composition> GetCompositionAsync()
+        {
+            var composition = new Composition()
+            {
+                Name = "This came from a Kontent provider."
+            };
+
+            return Task.FromResult(composition);
+        }
+
+        public Task<Fragment> GetFragmentAsync(string id)
+        {
+            var fragment = new Fragment()
+            {
+                Id = id,
+                Name = "This came from a Kontent provider"
+            };
+
+            return Task.FromResult(fragment);
+        }
+    }
+}
