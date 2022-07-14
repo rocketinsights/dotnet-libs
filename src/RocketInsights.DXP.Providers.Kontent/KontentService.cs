86b12fe87aa2b51be766f97560b7f9347b836242
John Askew <john.askew@deptagency.com>
2022-07-14 08:54:17 -0400

    Add reference implementation of IContextService within concrete service implementation(s).

--- src/RocketInsights.DXP.Providers.Kontent/KontentService.cs
+++ src/RocketInsights.DXP.Providers.Kontent/KontentService.cs
@@ -1,16 +1,30 @@
-﻿using RocketInsights.DXP.Models;
+﻿using RocketInsights.Contextual.Services;
+using RocketInsights.DXP.Models;
 using RocketInsights.DXP.Services;
+using System;
 using System.Threading.Tasks;
 
 namespace RocketInsights.DXP.Providers.Kontent
 {
     public class KontentService : ILayoutService, IContentService
     {
+        private IContextService ContextService { get; }
+
+        public KontentService(IContextService contextService)
+        {
+            ContextService = contextService;
+        }
+
         public Task<Composition> GetCompositionAsync()
         {
+            if(!ContextService.TryGetContext(out var context))
+            {
+                throw new Exception("Unable to retrieve a context");
+            }
+
             var composition = new Composition()
             {
-                Name = "This came from a Kontent provider."
+                Name = $"This came from a Kontent provider ({context.Culture.DisplayName})."
             };
 
             return Task.FromResult(composition);
@@ -18,10 +32,15 @@ namespace RocketInsights.DXP.Providers.Kontent
 
         public Task<Fragment> GetFragmentAsync(string id)
         {
+            if (!ContextService.TryGetContext(out var context))
+            {
+                throw new Exception("Unable to retrieve a context");
+            }
+
             var fragment = new Fragment()
             {
                 Id = id,
-                Name = "This came from a Kontent provider"
+                Name = $"This came from a Kontent provider ({context.Culture.DisplayName})."
             };
 
             return Task.FromResult(fragment);
